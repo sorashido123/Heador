ae29fb457d90e581047e09488e2705449bcbb2b0
Your Name <youremail@example.com>
2024-07-04 14:45:06 +0900

    Victory message added

--- hot_game.py
+++ hot_game.py
@@ -1,8 +1,5 @@
 import random
 
-name = input("Who are you?\n> ")
-print(f"Hello, {name}!")
-
 print("Tossing a coin...")
 heads = 0
 tails = 0
@@ -16,3 +13,8 @@ for i in range(1, 4):
         tails += 1
 
 print(f"Heads: {heads}, Tails: {tails}")
+
+if heads > tails:
+    print("You won")
+else:
+    print("You lost")
